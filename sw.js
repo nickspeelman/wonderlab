fa1a334e901fc1eab5ec38092c9a75897cbbd30a
Nick Speelman <nspeelman@gmail.com>
2026-08-06 19:19:17 -0400

    two axis fix, background fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'roscoe-wonder-lab-v16';
+const CACHE = 'roscoe-wonder-lab-v18';
 const ASSETS = [
   './', './index.html', './css/styles.css', './js/config.js', './js/app.js', './manifest.webmanifest',
   './assets/icons/icon-192.png', './assets/icons/icon-512.png'
